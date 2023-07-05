14b1ec60b88d1e66884388c225ef491a70e310b5
bhosalesubhas35@gmail.com <bhosalesubhas35@gmail.com>
2023-07-05 17:09:06 +0530

    aswin rebase

--- src/test/java/class_A/class_A.java
+++ src/test/java/class_A/class_A.java
@@ -2,8 +2,11 @@ package class_A;
 
 public class class_A {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 			System.out.println("class A");
+			
+			System.out.println("+++++++++ hello +++++++++++");
 	}
 
 }
